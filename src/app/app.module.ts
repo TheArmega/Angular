7cc69902cc6baf7685dccfca4e2866f1800c9fcd
Armega <email>
2021-01-25 11:55:50 +0100

    Componente lista de correos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './components/correo/correo.component';
+import { ListaCorreosComponent } from './components/lista-correos/lista-correos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CorreoComponent
+    CorreoComponent,
+    ListaCorreosComponent
   ],
   imports: [
     BrowserModule,
